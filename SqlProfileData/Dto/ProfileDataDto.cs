23da0112e9cabf42c2605475b0e9abdb9a195e12
Chris Russell <chris.russell@starcompliance.com>
2023-07-11 10:40:20 +0100

    Initial commit - retrieving profile data from db

---
+++ SqlProfileData/Dto/ProfileDataDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SqlProfileAnalysis.Dto
+{
+    public class ProfileDataDto
+    {
+        public int? EventClass { get; set; }
+        public string? TextData { get; set; }
+        public string? ApplicationName { get; set; }
+        public string? NTUserName { get; set; }
+        public string? LoginName { get; set; }
+        public int? CPU { get; set; }
+        public long? Reads { get; set; }
+        public long? Writes { get; set; }
+        public long? Duration { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+}
---
+++ SqlProfileData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+using SqlProfileAnalysis.Models;
+
+namespace SqlProfileAnalysis
+{
+    public static class Program
+    {
+        private static readonly string s_connectionStringEnvironmentVariable = "SQL_PROFILE_DB";
+
+        public static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(s_connectionStringEnvironmentVariable);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.Error.WriteLine(
+                    $"Connection string environment variable ({s_connectionStringEnvironmentVariable}) is not set");
+            }
+
+            else
+            {
+                Console.WriteLine(connectionString);
+                using var connection = new SqlConnection(connectionString);
+
+                var sql = "SELECT * FROM [InsiderReportGeneration - 2023-07-11]";
+
+                var profileData = connection.Query<SqlProfileData>(sql).ToList();
+
+                Console.WriteLine(profileData.Count);
+            }
+        }
+    }
+}
